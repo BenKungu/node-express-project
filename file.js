c3231a6f36eddd5b8c22e21443faa404843a83c5
BenKungu <benwaweru2013@gmail.com>
2023-07-03 01:23:27 +0300

    file systems

---
+++ file.js
@@ -0,0 +1,37 @@
+ const fs = require ('fs');
+// create a file
+
+
+fs.writeFile('example.txt',"this is an example",(err)=>{
+    if(err)
+    console.log(err);
+    else
+    console.log('file successfully created');
+    fs.readFile('example.txt','utf8' ,(err, file)=>{
+        if(err)
+        console.log(err);
+        else
+        console.log(file);
+    })
+});
+
+fs.rename('example.txt','example2.txt',(err)=>{
+    if (err)
+    console.logK(err);
+    else
+    console.log('successfully renamed the file')
+});
+
+fs.appendFile('example2.txt','some data being appended',(err)=>{
+    if(err)
+    console.log(err);
+    else
+    console.log('successfully added data to the file')
+});
+
+fs.unlink('example2.txt', (err)=>{
+    if(err)
+    console.log(err);
+    else
+    console.log('successfully deleted the file');
+})
