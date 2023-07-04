d62ee781ee9e5cff8df9b756711c3fb3ac34ff9b
BenKungu <benwaweru2013@gmail.com>
2023-07-04 18:54:34 +0300

    form data parsing

--- express.js
+++ express.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'static','index.html'));
@@ -13,7 +14,7 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{
     console.log(req.body);
     //database reaching task
-    res.send('successfully posted data');
+    res.json({success : true});
 })
 
 app.get('/waigata',(req,res)=>{
